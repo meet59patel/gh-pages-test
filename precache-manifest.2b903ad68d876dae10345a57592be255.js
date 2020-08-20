a046ac509943b5f7d6768802b2fd8532e29159bd
Meet Patel <patelmeet59@gmail.com>
2020-08-20 21:05:11 +0530

    Updates

---
+++ precache-manifest.2b903ad68d876dae10345a57592be255.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7704de7f2f80b5439ba54c81beb85e83",
+    "url": "/gh-pages-test/index.html"
+  },
+  {
+    "revision": "3168a6ed74f62225f9f0",
+    "url": "/gh-pages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/gh-pages-test/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-test/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3168a6ed74f62225f9f0",
+    "url": "/gh-pages-test/static/js/main.1f07910b.chunk.js"
+  },
+  {
+    "revision": "d483ffbd933d61d0b607",
+    "url": "/gh-pages-test/static/js/runtime-main.ee2e7bd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
